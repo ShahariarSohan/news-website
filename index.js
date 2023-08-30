6ffe0131f17889c62d5cb44effa59c8141cc00e2
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-30 20:14:40 +0600

    new data loaded

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const newsTabLoad = async () => {
 }
 newsTabLoad()
 //newsTabShow
+const newsContainer = document.getElementById('news-container')
 const newsTabShow = (newsCategories) => {
     newsCategories.forEach(category => {
         // console.log(category.category_id)
-        const newsContainer = document.getElementById('news-container')
         const newsTabDiv = document.createElement('div')
         newsTabDiv.innerHTML = `
         <a onclick="newsDataLoad('${category.category_id}')"class="tab text-xl font-bold ">${category.category_name}</a>
@@ -23,6 +23,13 @@ const newsTabShow = (newsCategories) => {
 const newsDataLoad = async (categoryId) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
     const data = await res.json()
-    console.log(data.data)
+    const details = data.data
+    newsCardShow(details)
 }
+//news card
+const newsCardShow = (details) => {
+    details.forEach(detail => {
+        console.log(detail)
 
+    })
+}
